cb7a083dfc90413ab09dc24b6d3103eb257c419e
mortalglitch <mortal.glitch@gmail.com>
2025-10-15 14:39:50 -0400

    added clock and delta time

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -14,6 +17,9 @@ def main():
 
         screen.fill("black")
         pygame.display.flip()
+
+        # End of loop actions
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
